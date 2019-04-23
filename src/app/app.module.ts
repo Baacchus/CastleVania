0f4a63de959ec5beaf1da43f909a9b5a8bb116a2
zoran <zoran.nadkov@gmail.com>
2019-04-23 14:06:39 +0200

    beta gameloop 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
