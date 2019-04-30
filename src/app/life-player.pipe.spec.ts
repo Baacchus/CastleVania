35b808b9a9a103a880c32b3a48c2b9509acc5ce9
Baacchus <36199891+Baacchus@users.noreply.github.com>
2019-04-30 13:27:36 +0200

    Revert "Feature/attack"

--- src/app/life-player.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LifePlayerPipe } from './life-player.pipe';
-
-describe('LifePlayerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LifePlayerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
