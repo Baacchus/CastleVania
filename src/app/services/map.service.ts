616196a3524ddbca12948942f06f179aaea65b7e
Damien Guyonnet <damien.guyonnet@gmail.com>
2019-05-02 10:38:44 +0200

    MAJ Monsters

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -13,9 +13,10 @@ const WALL = 3;
 export class MapService {
 
   public monsters: Monster[] = [
-    new Wolf(6, 8, 1, 1), new Wolf(8, 5, 1, 1), new Wolf(15, 8, 1, 1), new Wolf(25, 8, 1, 1), new Wolf(27, 8, 1, 1), new Wolf(29, 8, 1, 1), new Wolf(31, 8, 1, 1),
-     new Ghost(75, 7.5, 0, 0), new Ghost(75, 7.5, 1, 2), new Ghost(78, 7.5, 2, 2), new Wolf(25, 8, 1, 1), new Ghost(82, 7.5, 2, 2),
-     new Beast(132, 7.3, 0, 0), new Beast(130, 7.3, 0, 0), new Beast(125, 7.3, 0, 0)];
+    new Wolf(6, 8, 1, 1), new Wolf(8, 5, 1, 1), new Wolf(15, 8, 1, 1), new Wolf(25, 8, 1, 1), new Wolf(27, 8, 1, 1),
+    new Wolf(29, 8, 1, 1), new Wolf(31, 8, 1, 1), new Wolf(97, 8, 1, 1), new Wolf(115, 8, 1, 1), new Wolf(100, 8, 1, 1), new Wolf(105, 8, 1, 1),
+    new Ghost(93, 3.5, 0, 0), new Ghost(75, 7.5, 0, 0), new Ghost(75, 7.5, 1, 2), new Ghost(78, 7.5, 2, 2), new Wolf(25, 8, 1, 1), new Ghost(82, 7.5, 2, 2),
+    new Beast(130, 7.3, 0, 0), new Beast(125, 7.3, 0, 0)];
                             
   constructor() { }
 
