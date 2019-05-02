ca7101a66c98aeb7610416d2f96f246f4439931c
zoran <zoran.nadkov@gmail.com>
2019-05-02 08:51:16 +0200

    ajout monstre

--- src/app/monster/monster.ts
+++ src/app/monster/monster.ts
@@ -10,15 +10,19 @@ export class Monster {
 }
 
 export class Wolf extends Monster {
-
     constructor(public monsterX: number, public monsterY: number, public amplitude: number, direction: number) {
         super('wolf', monsterX, monsterY, amplitude, direction)
-
     }
 }
 
 export class Ghost extends Monster {
     constructor(public monsterX: number, public monsterY: number, public amplitude: number, direction: number) {
         super('ghost', monsterX, monsterY, amplitude, direction)
+    }
+}
+
+export class Beast extends Monster {
+    constructor(public monsterX: number, public monsterY: number, public amplitude: number, direction: number) {
+        super('beast', monsterX, monsterY, amplitude, direction)
     }
 }
--- src/app/services/state-machine.service.ts
+++ src/app/services/state-machine.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Wolf, Ghost, Monster } from '../monster/monster';
+import { Wolf, Ghost, Monster, Beast } from '../monster/monster';
+
 
 export const MOVE_LEFT = 1;
 export const MOVE_RIGHT = 2;
@@ -21,7 +22,7 @@ export class StateMachineService {
 
   lifePlayer: number = 4;
   public move: any = 0;
-  public monsters: Monster[] = [new Wolf(4, 8, 3, 1), new Wolf(8,5,2,2), new Ghost(15,3,0,0)]
+  public monsters: Monster[] = [new Wolf(4, 8, 3, 1), new Wolf(8,5,2,2), new Ghost(15,3,0,0),  new Beast(4,5,0,0)]
   public startTime: Date;
   public gameDuration: number = 0;
   public endTime: Date;
@@ -32,5 +33,3 @@ export class StateMachineService {
     this.moveState = state;
   }
 }
-
-
